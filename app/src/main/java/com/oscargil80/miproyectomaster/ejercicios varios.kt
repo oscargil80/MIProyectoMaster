e42d44ac9bd990cb44850af64fd46f6a7261ef2c
Oscargil80 <oscargil80@gmail.com>
2024-02-07 14:22:17 -0400

    Agregando los anchivos anteriores de las practicas

--- app/src/main/java/com/oscargil80/miproyectomaster/ejercicios varios.kt
+++ app/src/main/java/com/oscargil80/miproyectomaster/ejercicios varios.kt
@@ -4,33 +4,47 @@ package com.oscargil80.miproyectomaster
 //pf = valor de venta - IGV
 
 fun main (){
-    var radio = 5.0
-    //println("El area es "+areaTri(5, 6))
-    //println("El area es  ${radio.pow(2)}")
-    var valVen = 100 ; var IGV = (18.0/100)*100;   var pf = valVen - IGV
-    //println("El IGV = $IGV")
-    //println("El Precio Final es = $pf")
+            //-----------------------
+            //var radio = 5.0
+            //println("El area es "+areaTri(5, 6))
+            //println("El area es  ${radio.pow(2)}")
+            //-------------------------------
 
-    // De horas a minutos
-    var hora:Int = 5
-//    println("$hora Horas son "+ aMin(hora)+" Minutos ")
-//    println("$hora Horas son "+ aSeg(hora)+" Segundos ")
-    var nroMen:Int = 10 ;    var nroMay:Int = 19
-    println( "La cantida de numeros entre $nroMen y $nroMay  es "+hayNum(nroMen, nroMay))
+            //var valVen = 100 ; var IGV = (18.0/100)*100;   var pf = valVen - IGV
+            //println("El IGV = $IGV")
+            //println("El Precio Final es = $pf")
+
+            // De horas a minutos
+            //var hora:Int = 5
+            //    println("$hora Horas son "+ aMin(hora)+" Minutos ")
+            //    println("$hora Horas son "+ aSeg(hora)+" Segundos ")
+            //    var nroMen:Int = 10 ;    var nroMay:Int = 19
+            // println( "La cantida de numeros entre $nroMen y $nroMay  es "+hayNum(nroMen, nroMay))
+
+//         var n = 10.0
+//         println("La sum de cuadrados de $n = " + sCuadrados(n))
+
+    var hTrabajadas:Int = 50
+    var sPorHora = 18.5
+    println("El sueldo de este trabajador luego de haber trabajado $hTrabajadas Horas con un Sueldo por Hora de $sPorHora es  de "+cSalario(hTrabajadas, sPorHora))
 }
 
+fun cSalario(hTrabajadas:Int, sPorHora:Double):Double{
+    return hTrabajadas * sPorHora
+}
+
+fun sCuadrados(n:Double):Double{
+    return  (n*(n+1) ) * ( 2*n + 1)  /6
+}
 fun areaTri(base:Int, altura:Int):Int{
     return (base*altura)/2
 }
-
 fun aMin(hora:Int):Int{
     return hora*60
 }
-
 fun aSeg(hora:Int):Int{
     return (aMin(hora)*60)
 }
-
 fun hayNum(nroMen:Int, nroMay:Int):Int{
     return (nroMay-nroMen) - 1
 }
