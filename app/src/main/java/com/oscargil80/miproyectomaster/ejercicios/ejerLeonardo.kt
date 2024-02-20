3d765e420d7f1e84149aad115bbb2497ef2bbcb8
Oscargil80 <oscargil80@gmail.com>
2024-02-20 00:37:16 -0400

    Agregando los anchivos anteriores de las practicas

--- app/src/main/java/com/oscargil80/miproyectomaster/ejercicios/ejerLeonardo.kt
+++ app/src/main/java/com/oscargil80/miproyectomaster/ejercicios/ejerLeonardo.kt
@@ -6,9 +6,42 @@ fun main(){
     //println("El numero que has introducido es "+parImpar(readLine()!!.toInt() ))
     /////
   ///Ejercicio 2
-    var lista = arrayOf(1f,4f,15f)
-    println("La suma de los numeros introducidos dan un total final "+Sumar(lista))
+    //var lista = arrayOf(1f,4f,15f)
+    //println("La suma de los numeros introducidos dan un total final "+Sumar(lista))
+    //////Ejercicio 3
+//    var numDes = arrayOf(10,15,20,2,7,8,6,28,90,50,66,77)
+//    println("Los numeros del arreglo  que son pares son  ---->  ${ordPar(numDes).contentToString()}  y estan ordenados ")
+//    ordPar(numDes)
+////// Ejercicio 4
+//    println("... Example One ...")
+   // println((0..2).random())
+    println("Introduzca un numero pasa bes si aciertas la posicion de la X ")
+    println("la posicion que intentas de adivinar es "+   adivinaPos(readLine()!!.toInt()))
 }
+
+ fun adivinaPos(num:Int):String{
+     var array = arrayOf("0", "0","0")
+     var aleatorio:Int = (0..2).random()
+     array.set(aleatorio, "x")
+     println(array.contentToString())
+     if (num ==   array.indexOf("x"))   return "Correcto"  else  return "Incorrecto"
+ }
+
+
+
+
+
+fun ordPar(lista:Array<Int>):Array<Int>
+{
+    var numOrd = arrayOf<Int>()
+
+    for (i in lista)
+        if(i%2==0 )    numOrd += i
+    numOrd.sort()
+    return numOrd
+}
+
+
 
 fun parImpar (num: Int): String {
     var res = "impar"
@@ -22,3 +55,4 @@ fun Sumar (lista:Array<Float>):Float{
         tot = tot+value
     return tot
 }
+
