710fbba3d973eb1d9d19ef764f45bc9cd8f5ec62
Oscargil80 <oscargil80@gmail.com>
2024-02-20 14:29:06 -0400

    Agregando los anchivos anteriores de las practicas

--- app/src/main/java/com/oscargil80/miproyectomaster/ejercicios/ejerLeonardo.kt
+++ app/src/main/java/com/oscargil80/miproyectomaster/ejercicios/ejerLeonardo.kt
@@ -1,17 +1,17 @@
 package com.oscargil80.miproyectomaster.ejercicios
 
 fun main(){
-    //Ejercicio Numero 1
-    println("Introduzca el valor para verificar si es Par  o Impar")
-    println("El numero que has introducido es "+parImpar(readLine()!!.toInt() ))
-    /////
-  ///Ejercicio 2
-  var lista = arrayOf(1f,4f,15f)
-   println("La suma de los numeros introducidos dan un total final "+Sumar(lista))
-    //////Ejercicio 3
-    var numDes = arrayOf(10,15,20,2,7,8,6,28,90,50,66,77)
-    println("Los numeros del arreglo  que son pares son  ---->  ${ordPar(numDes).contentToString()}  y estan ordenados ")
-    ordPar(numDes)
+//    //Ejercicio Numero 1
+//    println("Introduzca el valor para verificar si es Par  o Impar")
+//    println("El numero que has introducido es "+parImpar(readLine()!!.toInt() ))
+//    /////
+//  ///Ejercicio 2
+//  var lista = arrayOf(1f,4f,15f)
+//   println("La suma de los numeros introducidos dan un total final "+Sumar(lista))
+//    //////Ejercicio 3
+//    var numDes = arrayOf(10,15,20,2,7,8,6,28,90,50,66,77)
+//    println("Los numeros del arreglo  que son pares son  ---->  ${ordPar(numDes).contentToString()}  y estan ordenados ")
+//    ordPar(numDes)
 ////// Ejercicio 4
     println("Introduzca un numero pasa bes si aciertas la posicion de la X ")
     println("la posicion que intentas de adivinar es "+   adivinaPos(readLine()!!.toInt()))
@@ -19,9 +19,11 @@ fun main(){
 
  fun adivinaPos(num:Int):String{
      var array = arrayOf("0", "0","0")
+//     array.shuffle() para desordenar laos valores //     array.forEach { //         print(" $it,") //     }
      var aleatorio:Int = (0..2).random()
      array.set(aleatorio, "x")
-     println(array.contentToString())
+    // println(array.contentToString())
+
      if (num ==   array.indexOf("x"))   return "Correcto"  else  return "Incorrecto"
  }
 
