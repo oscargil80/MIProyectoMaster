5f32f9c30c2a3e4a567ed602447d7904c6812d92
Oscargil80 <oscargil80@gmail.com>
2024-02-22 14:12:13 -0400

    Agregando los anchivos anteriores de las practicas

---
+++ app/src/main/java/com/oscargil80/miproyectomaster/ejeClasObj/Persona.kt
@@ -0,0 +1,22 @@
+package com.oscargil80.miproyectomaster.ejeClasObj
+
+class Persona( var nombre: String,  var edad: Int , esEstudiante: Boolean = false)  {
+
+    init {
+       println("Nombre $nombre")
+        println("Edad $edad")
+        println("Estudiante? $esEstudiante")
+
+    }
+
+    fun caminar(){
+        println("Caminando ")
+    }
+
+    fun comer(){
+        println("Comiendo")
+    }
+
+
+
+}
---
+++ app/src/main/java/com/oscargil80/miproyectomaster/ejeClasObj/Principal.kt
@@ -0,0 +1,23 @@
+package com.oscargil80.miproyectomaster.ejeClasObj
+
+fun main(){
+
+    var oscar = Persona("Oscar", 44)
+    oscar.caminar()
+
+
+    var linin = Persona("Linin", 48)
+    linin.caminar()
+
+
+    var Edys = Persona("Edys", 72)
+    Edys.caminar()
+
+    var Justyn  = Persona("Justyn", 16, true)
+    Justyn.caminar()
+
+
+
+
+
+}
