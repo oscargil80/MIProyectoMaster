6b9df46edfa2cab85a0607d173b730aabfc1526a
Oscargil80 <oscargil80@gmail.com>
2024-03-07 16:46:56 -0400

    Agregando los anchivos anteriores de las practicas

--- app/src/main/java/com/oscargil80/miproyectomaster/GLOSARIO/For While Do While.kt
+++ app/src/main/java/com/oscargil80/miproyectomaster/GLOSARIO/For While Do While.kt
@@ -5,14 +5,49 @@ package com.oscargil80.miproyectomaster.GLOSARIO
 fun main (){
 
                                                                                                      // For Loop
+   /* for (i in 1..40)
+    {
+       // println("For Sencillo con break->$i")
+        if(i ==10)
+            break
+    }*/
+                                                                                                                                        // // For Anidado
+   /* for (i in 1..4)
+    {
+         for (j in 1..4){
+            println("$i   $j")
+            if (i == 2 && j ==2 ){
+                println("Aqui")
+            }
+         }
+     }*/
+
+                                                                                                                                         // // For Anidado con break
+    /*loop@ for (i in 1..4)
+    {
+        for (j in 1..4){
+            println("$i   $j")
+            if (i == 2 && j ==2 ){
+                println("Aqui")
+                break@loop    //// El break no funciona sino se identifica
+            }
+        }
+    }*/
+
+                                                                                                                                              // // For Anidado con continue
     for (i in 1..4)
     {
-     //   println("Hello")
+        if(i==3){
+            continue
+        }
+        println("i-->" +i)
     }
-    //println("While \n")
+
+
+    println("While \n")
                                                                                                                                          //En arreglos
     var arr = arrayOf(10,20,30,40,50)
-
+/*
     for (i in arr)
         println("For sencillo Valor"+i)
 
@@ -22,9 +57,10 @@ fun main (){
     for ((i, value) in arr.withIndex()) {
         println("Pidiendo 2 valores, Indices = " + i)
         println("Pidiendo 2 valores, valor  =" + value)
-    }                                                                                                                                    // forEach en arreglos
-    arr.forEach { println("ForEach sencillo  "+it) }
-    arr.forEach { if (it >20 ) println("ForEach con condicion "+it) }
+    }      */
+                                                                                                                                        // forEach en arreglos
+   /* arr.forEach { println("ForEach sencillo  "+it) }
+    arr.forEach { if (it >20 ) println("ForEach con condicion "+it) }*/
 
                                                                                                      // While
     var i:Int = 1
