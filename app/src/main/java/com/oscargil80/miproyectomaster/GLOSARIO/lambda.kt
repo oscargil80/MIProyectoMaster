6df1e6f25443fd9485984867e28f39843826c3aa
Oscargil80 <oscargil80@gmail.com>
2024-03-18 15:05:28 -0400

    Agregando los anchivos anteriores de las practicas

---
+++ app/src/main/java/com/oscargil80/miproyectomaster/GLOSARIO/lambda.kt
@@ -0,0 +1,29 @@
+package com.oscargil80.miproyectomaster.GLOSARIO
+
+fun main() {
+    var res  = {a:Int, b: Int -> a+b}
+    println(res(2,4))
+
+    // Lo Mismo multiline
+
+    var res2  = { a: Int, b: Int ->
+        a + b
+    }
+
+    println(res2(2,4))
+
+
+
+    val lista = listOf<Int>(5,2,4,8,9,7,10)
+    var total = 0
+    val  sumaPrecio:(List<Int>) -> Int = { lista -> lista.forEach{ total += it };total}
+    val resta:(List<Int>) -> Int = {lista.forEach {total *=it};total }
+    println(sumaPrecio(lista))
+    println(resta(lista))
+
+
+    }
+
+
+
+
